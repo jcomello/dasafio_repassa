e84e7cb2e061d2d2745547757b6b89c3602d2744
João Carreira de Oliveira e Mello <joao.mello@nexaas.com>
2020-02-21 19:42:58 -0300

    Add association spec to employee

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
+  describe "associations" do
+    it { is_expected.to have_many(:perfoermance_evaluations) }
+  end
+
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
   end
